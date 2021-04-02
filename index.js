4b1263d5d7f564339863026adf39b772bbf2d086
Denny Srun <dennysrun@gmail.com>
2021-04-02 05:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function yourAnimal() {
    var animal = 'cat'
   return animal
 }
-
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
